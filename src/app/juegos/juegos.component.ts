717b86da6d9b8ccab1ef560f9426c5c407d494fc
Pablo Eguilaz Perez IES Doctor Fleming <pabloep17@educastur.es>
2024-01-25 19:25:57 +0100

    Update

--- src/app/juegos/juegos.component.ts
+++ src/app/juegos/juegos.component.ts
@@ -6,8 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./juegos.component.css']
 })
 export class JuegosComponent {
+
   public juegos: Array<any> = [
     { name: "Piedra Papel Tijera", img: "https://media.istockphoto.com/id/1269211670/es/vector/rock-papel-tijeras-conjunto-de-iconos-de-partes-del-cuerpo.jpg?s=612x612&w=0&k=20&c=GUYVPaBrY6ZJQUIcJKj0n4uXee90wziYxg6YP1ZBcs4=", href: "juegos"},
     { name: "El tres en raya", img: "https://www.realsporting.com/images/gijon-logo-1136f311e33d28a5453c84669240e4cb.png?vsn=d", href: "juegos"},
   ];
+
+  buscador:string = "No se que poner"
+
+  changeBuscardor(value:string) {
+    this.buscador = value
+  }
+
 }
