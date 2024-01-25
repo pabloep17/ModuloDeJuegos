26cd6cb9c03bfe4b34e78b18f0f8b8acb7b133c8
Pablo Eguilaz <pabloeguilazperez@gmail.com>
2024-01-25 20:51:04 +0100

    Update

--- src/app/juegos/juegos.component.ts
+++ src/app/juegos/juegos.component.ts
@@ -9,7 +9,7 @@ export class JuegosComponent {
 
   public juegos: Array<any> = [
     { name: "Piedra Papel Tijera",descripcion: "No se que puedo poner aqui", img: "https://media.istockphoto.com/id/1269211670/es/vector/rock-papel-tijeras-conjunto-de-iconos-de-partes-del-cuerpo.jpg?s=612x612&w=0&k=20&c=GUYVPaBrY6ZJQUIcJKj0n4uXee90wziYxg6YP1ZBcs4=", href: "juegos"},
-    { name: "Real Sporting",descripcion: "Equipo de Primera", img: "https://www.realsporting.com/images/gijon-logo-1136f311e33d28a5453c84669240e4cb.png?vsn=d", href: "juegos"},
+    { name: "",descripcion: "", img: "", href: "juegos"},
   ];
 
   buscador:string = ""
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from "@vercel/speed-insights"
 
 import { AppModule } from './app/app.module';
 
+inject();
+injectSpeedInsights();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
