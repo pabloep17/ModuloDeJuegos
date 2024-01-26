67b4e2309b593663cd801505ec7da0773583b57d
Pablo Eguilaz <pabloeguilazperez@gmail.com>
2024-01-26 23:53:51 +0100

    update

--- src/app/desarrollador/desarrollador.component.ts
+++ src/app/desarrollador/desarrollador.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class DesarrolladorComponent {
 
+  firstname:string = ""
+  lastname:string = ""
+  email:string = ""
+  body:string = ""
+
+  send() {
+    var formdata = new FormData();
+    formdata.append("texto", `${this.firstname} ${this.lastname} / ${this.email}`);
+    formdata.append("body", this.body);
+
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+    };
+
+    fetch("https://api.pabloeguilaz.es/", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+      }
+
 }
