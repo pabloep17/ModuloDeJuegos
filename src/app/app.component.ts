8da44c83a6023c325e7476f7253dea7a724bc839
Pablo Eguilaz <pabloeguilazperez@gmail.com>
2024-01-26 12:10:47 +0100

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'modulodejuegos';
+
+  pageTitle: string = '';
+
+  ngOnInit() {
+    // Obtén el título de la página actual y asígnalo a la propiedad pageTitle
+    this.pageTitle = document.title;
+  }
+
 }
