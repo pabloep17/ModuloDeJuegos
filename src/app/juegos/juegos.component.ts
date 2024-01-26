24e9bcaff47faa5c0185eedea647748321fdb437
Pablo Eguilaz <pabloeguilazperez@gmail.com>
2024-01-26 12:05:52 +0100

    Update

--- src/app/juegos/juegos.component.ts
+++ src/app/juegos/juegos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-juegos',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./juegos.component.css']
 })
 export class JuegosComponent {
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('Juegos');
+  }
 
   public juegos: Array<any> = [
     { name: "Piedra Papel Tijera",descripcion: "El juego de piedra papel tijera", img: "https://media.istockphoto.com/id/1269211670/es/vector/rock-papel-tijeras-conjunto-de-iconos-de-partes-del-cuerpo.jpg?s=612x612&w=0&k=20&c=GUYVPaBrY6ZJQUIcJKj0n4uXee90wziYxg6YP1ZBcs4=", href: "juegos"},
