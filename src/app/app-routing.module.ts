aa22005589f219164ff83967b5b34a9df2fd0787
Pablo Eguilaz <pabloeguilazperez@gmail.com>
2024-01-26 14:09:46 +0100

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { JuegosComponent } from './juegos/juegos.component';
+
 const routes: Routes = [
+  { path: 'inicio', component: JuegosComponent},
   { path: 'juegos', component: JuegosComponent},
-  { path: 'inicio', component: JuegosComponent}
+  { path: 'informacion', component: JuegosComponent},
+  { path: 'desarrollador', component: JuegosComponent},
+  { path: 'version', component: JuegosComponent},
 ];
 
 @NgModule({
