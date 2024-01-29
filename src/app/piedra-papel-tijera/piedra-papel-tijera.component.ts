5e54a455570d220c450f9eda57be54bee1bfaa56
Pablo Eguilaz Perez IES Doctor Fleming <pabloep17@educastur.es>
2024-01-29 20:31:26 +0100

    pedra papel ou tijera

--- src/app/piedra-papel-tijera/piedra-papel-tijera.component.ts
+++ src/app/piedra-papel-tijera/piedra-papel-tijera.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class PiedraPapelTijeraComponent {
 
+  iaMove:number = 0
+  playerMove:number = 0
+
+  ngOnInit() {
+    window.alert("H")
+  }
+
 }
