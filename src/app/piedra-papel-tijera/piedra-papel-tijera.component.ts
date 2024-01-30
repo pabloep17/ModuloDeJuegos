180a575d3b16749bdf54fd32cb9ffa5cdf62f4b6
Pablo Eguilaz Perez IES Doctor Fleming <pabloep17@educastur.es>
2024-01-30 21:09:55 +0100

    Update

--- src/app/piedra-papel-tijera/piedra-papel-tijera.component.ts
+++ src/app/piedra-papel-tijera/piedra-papel-tijera.component.ts
@@ -7,11 +7,59 @@ import { Component } from '@angular/core';
 })
 export class PiedraPapelTijeraComponent {
 
-  iaMove:number = 0
-  playerMove:number = 0
+  localStorageData:string | null | any = "" 
+
+  computerSelection:number = 0
+  computerSelectionImage:string | undefined = ""
+
+  playerSelectionImage:string | undefined = ""
+
+  msg:string = ""
+
+  setPlayerSelection(v:number) {
+    
+    this.computerSelection = this.getRandomArbitrary(1, 3)
+
+    this.computerSelectionImage = this.selecciones.get(this.computerSelection)
+    this.playerSelectionImage = this.selecciones.get(v)
+
+    switch(this.computerSelection) {
+      case 1:
+        this.msg = v == 1 ? "Empate" : v == 2 ? "Jugador gana" : "Ordenador gana"
+        break
+      case 2:
+        this.msg = v == 2 ? "Empate" : v == 3 ? "Jugador gana" : "Ordenador gana"
+        break
+      case 2:
+        this.msg = v == 3 ? "Empate" : v == 1 ? "Jugador gana" : "Ordenador gana"
+        break
+    }
+
+    if (this.msg == "Jugador gana") {
+      this.localStorageData.result.player += 1
+    } else if (this.msg == "Ordenador gana") {
+      this.localStorageData.result.computer += 1
+    }
+
+    localStorage.setItem("lastGame", JSON.stringify(this.localStorageData))
+    
+  }
+
+  selecciones:Map<number, string> = new Map()
 
   ngOnInit() {
-    window.alert("H")
+    this.selecciones.set(1, "piedra")
+    this.selecciones.set(2, "papel")
+    this.selecciones.set(3, "tijera")
+
+    this.localStorageData = localStorage.getItem("lastGame")
+    this.localStorageData = JSON.parse(this.localStorageData)
+
   }
+
+  getRandomArbitrary(min: number, max: number) {
+    return Math.trunc(Math.random() * (max - min) + min);
+  }
+
 
 }
