695abef4502b4577b4acbce222b54502e058f275
Pablo Eguilaz <pabloeguilazperez@gmail.com>
2024-02-09 00:30:29 +0100

    Update

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,7 +1,12 @@
 export const cv: any = [
     {
+        "title": "IES Carmen y Severo Ochoa",
+        "interval": "2020 - 2022",
+        "description": "Bachiller cientifico tecnológico"
+    },
+    {
         "title": "Desarrollador Web",
-        "interval": "2020 - Actualidad",
+        "interval": "2022 - Actualidad",
         "description": "Desarrollo de aplicaciones web con Angular, React, Node.js, Express, MongoDB, MySQL, Firebase, etc."
     }
 ]
