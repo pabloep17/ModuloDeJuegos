db4c90f1667d9a7fc688f4cbeb794c61f99f4b50
Pablo Eguilaz <pabloeguilazperez@gmail.com>
2024-02-09 15:29:12 +0100

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fondo: "#f7f7f7",
+        fondo_dark: "#000000",
+        section: "#FFFFFF",
+        section_dark: "#0a0a0a"
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
