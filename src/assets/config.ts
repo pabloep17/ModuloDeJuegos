5ddf8432785ac44ce84d9fc3c6da7eccc21e2aa3
Pablo Eguilaz <pabloeguilazperez@gmail.com>
2024-02-09 02:12:32 +0100

    Update

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -8,14 +8,12 @@ export const cv: any = [
     {
         title: "Desarrollador App Móviles",
         interval: "2022 - Actualidad",
-        description: "Desarrollo de aplicaciones móviles con Java, Angular, Ionic, Firebase, Swift, Kotlin, Flutter, React Native, etc.",
-        languages: ["Java", "Swift", "Angular", "Python"],
+        description: "Desarrollo de aplicaciones móviles con Java, Angular, Ionic, Firebase, Swift, Kotlin, Flutter, React Native, etc."
     },
     {
         title: "IES Carmen y Severo Ochoa",
         interval: "2020 - 2022",
-        description: "Bachiller cientifico tecnológico",
-        languages: ["Java", "Swift", "Angular", "Python"]
+        description: "Bachiller cientifico tecnológico"
     }
 ]
 
