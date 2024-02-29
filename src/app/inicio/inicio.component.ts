a614d17a514d305e1a0860a2db5359dffcf2cad4
Pablo Eguilaz <pabloeguilazperez@gmail.com>
2024-02-29 19:23:14 +0100

    Update

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -35,7 +35,7 @@ export class InicioComponent {
     })
     */
 
-    this.countdownDate = new Date('2024-03-08T20:00:00');
+    this.countdownDate = new Date('2024-04-10T00:00:00');
     this.updateCountdown()
 
   }
